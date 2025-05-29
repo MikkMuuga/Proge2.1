9d196a7c4dd4254d1d8a1fdcfbb20419a087a54d
Mikk <gregormikk@gmail.com>
2025-05-29 17:58:10 +0300

    ye

--- Proge2.1/Data/Machines.cs
+++ Proge2.1/Data/Machines.cs
@@ -5,6 +5,9 @@ namespace Proge2._1.Data
     public class Machines: Entity
     {
         [Required]
+        [Key]
+        public int id { get; set; }  
+        [Required]
         [StringLength(25)]
         public string Workers { get; set; }
         public String Supervision { get; set; }
